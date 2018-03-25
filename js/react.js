b5554d23caf58aa22cf4e936302d2288eb669ee1
Sherwin Suva <sherwinsuva@gmail.com>
2018-03-25 14:23:42 +0800

    Update react.js

--- js/react.js
+++ js/react.js
@@ -13,11 +13,29 @@ class SearchBar extends React.Component{
 			);
 	}
 }
+class Movies extends React.Component{
+	render(){
+		return(
+				<div class="movieContainer">
+					<a href="#">{this.props.title}</a>
+				</div>
+			);
+	}
+}
 
 class UpcomingMovies extends React.Component{
 	render(){
+		const upcomingMovieList = this.props.movieList.map((movies) => {
+			<li>
+				<Movies 
+					title={movies.results.title}
+					overview={movies.results.overview} />
+			</li>
+		});
 		return(
-				<div></div>
+				<div>
+					<ul>upcomingMovieList</ul>
+				</div>
 			);
 	}
 }
@@ -26,21 +44,41 @@ class MovieApp extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			listOfComments: [],
+			movieList: [],
 			showComments: false
 		}
+	}
+	componentWillMount(){
+		this.ajaxCall();
 	}
 	render(){
 		return(
 				<div>
           <h2 className="title">Movies</h2>
           <SearchBar />
-          <UpcomingMovies />
+          <UpcomingMovies movieList={this.props.movieList} />
           		</div>
           
 			);
 	}
+	getUpcomingMovie(){
 
+	}
+	getPopularMovie(){
+
+	}
+	//Not sure
+	ajaxCall(movieType){
+		let movieLink = "https://api.themoviedb.org/3/movie/" 
+						+ "upcoming" + "?api_key=ff9d34ddaaebff2b1a6100d54346c1a7&language=en-US&page=1";
+		$.ajax({
+			method: 'GET',
+			url: movieLink,
+			success: (movieList) => {
+				this.setState({movieList});
+			}
+		});
+	}
 }
 
 ReactDOM.render(<MovieApp  />, document.getElementById("main"));
