616784f3a7e95f0ba9d045729b3f9358fc9ed2db
Sherwin Suva <sherwinsuva@gmail.com>
2018-04-02 17:47:52 +0800

    Update react.js

--- js/react.js
+++ js/react.js
@@ -1,61 +1,127 @@
+//SearchBar Child
+class Result extends React.Component{
+	render(){
+		return(
+			<a href="#" className="suggestedList">{this.props.title}</a>
+			);
+	}
+}
 class SearchBar extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			movies: ''
+			movies: '',
+			showSugesstion: false
 		}
+		this.handleFilterChange = this.handleFilterChange.bind(this);
+		this.onFocus = this.onFocus.bind(this);
+		//this.onBlur = this.onBlur.bind(this);
 	}
+	handleFilterChange(e){
+    	this.props.onFilterChange(e.target.value);
+    }
+    onFocus(){
+    	this.setState({
+    		showSugesstion: this.state.showSugesstion
+    	});	
+    } 
+    /**onBlur(){
+    	this.setState({
+    		showSugesstion: false
+    	});	
+    }**/
 	render(){
-		return(
-				<form>
-					<input type="text" />
-				</form>
-			);
-	}
-}
-
-class Movies extends React.Component{
-	render(){
-		let bodyText = this.props.overview.substr(0, 90);
-		bodyText = bodyText.substr(0, Math.min(bodyText.length, bodyText.lastIndexOf(" "))) + "...";
+		const movieLists = this.props.searchMovie;
+		console.log(movieLists.length);
+		const rows = [];
+		let displaySuggestion;
+		if(movieLists.length < 1){
+			displaySuggestion = {
+				display: 'none'
+			}
+		}else{
+			displaySuggestion = {
+				display: 'block'
+			}
+		}
+		this.props.movieList.slice(0,7).forEach((movie) => {
+				rows.push(
+						<li>
+							<Result title={movie.title} 
+									key={movie.title}
+							/>
+						</li>
+					);
+			});
 		
 		return(
-				<div className="movieInfo">
+			<div className="searchSeaction">
+				<form className="searchBox">
+					<input className="searchBar"
+					type="text"
+					placeholder="Search Movie.." 
+					value={this.props.searchMovie} 
+					onChange={this.handleFilterChange} 
+					onFocus={this.onFocus}
+					//onBur={this.onBlur}
+					/>
+					<span className="suggestBox" 
+							style={displaySuggestion}>
+						<ul>{rows}</ul>
+					</span>
+				</form>
+			</div>
+			);
+	}
+
+}
+
+//MovieCategory Child
+class Movies extends React.Component{
+	render(){
+		let bodyText = this.props.overview.substr(0, 120);
+		bodyText = bodyText.substr(0, bodyText.lastIndexOf(" ")).concat("...");
+		
+		return(
+				<div className="movieBoxInfo">
 					<div className="infoWrapper">
-					<p className="boxTitle">{this.props.title}</p>
-					<p className="boxOverview">{bodyText}</p>
+						<p className="boxTitle">{this.props.title}</p>
+						<p className="boxRating">Rating: {this.props.rating}</p>
+						<p className="boxOverview">{bodyText}</p>
 					</div>
 				</div>
 			);
 	}
 }
 
-class UpcomingMovies extends React.Component{
+class MovieCategory extends React.Component{
 constructor(props){
 		super(props);
 		this.state={
 			movieList: []
 		}
 	}
-  componentDidMount(){
+  componentWillMount(){
 		this.ajaxCall();
 	}
 	render(){
 		let resultLink = this.state.movieList.slice(0,5);
 		const upcomingMovieList = resultLink.map((movies) => {
 			const backgroundImage = {
-				backgroundImage: `url(http://image.tmdb.org/t/p/w185/${movies.backdrop_path})`,
+				backgroundImage: `url(http://image.tmdb.org/t/p/w300/${movies.backdrop_path})`,
 				backgroundPosition: "center",
 				backgroundRepeat: "no-repeat",
-				width: "180px",
-				height: "180px"	
+				width: "220px",
+				height: "220px"	
 			};
 			return(
-			<li className="movieList">
+			<li className="movieCategoryList">
 				<a href="#" className="moviesBox" style={backgroundImage} >
 				<Movies 
 					title={movies.title}
-					overview={movies.overview} />
+					overview={movies.overview}
+					rating={movies.vote_average} />
+
 				</a>
 			</li>
 			);
@@ -68,17 +134,11 @@ constructor(props){
 	}
   
   ajaxCall(){
+  		const categoryLink = this.props.url; 
+  		const apiKey = "ff9d34ddaaebff2b1a6100d54346c1a7";
 		let movieLink = "https://api.themoviedb.org/3/movie/" 
-						+ this.props.url + "?api_key=ff9d34ddaaebff2b1a6100d54346c1a7&language=en-US&page=1";
-		/**$.ajax({
-			method: 'GET',
-			url: movieLink,
-			success: (movieList) => {
-				console.log(movieList);
-				this.setState({movieList: movieList.results});
-			}
-		});**/
-
+						+ categoryLink + "?api_key=" + apiKey + "&language=en-US&page=1";
+		console.log(movieLink);
 		fetch(movieLink).
 		then((Response) => Response.json()).
 		then((findmovie) => {
@@ -92,33 +152,72 @@ class MovieApp extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			movieList: [],
+			query: '',
+			movieList: []
 		}
+		this.handleFilter = this.handleFilter.bind(this);
+	}
+	componentWillMount(){
+		this.fetchData();
 	}
 	render(){
+
+		let movieLists = this.state.movieList;
+		console.log(movieLists);
 		return(
-				<div>
-          <h2 className="title">Movies</h2>
-          <SearchBar />
-	<h2>Upcoming</h2>
-          <UpcomingMovies url="upcoming"  />
-	<h2>Popular</h2>
-	<UpcomingMovies url="popular"  />		
-	<h2>Top Rated</h2>
-	<UpcomingMovies url="top_rated"  />
-          		</div>
+			<div>
+				<header className="mainHeader">
+					<div className="centerWrapper cfix">
+				        <h1 className="logo">MovieInfo</h1>
+				        <SearchBar 
+				          onFilterChange={this.handleFilter} 
+				          searchMovie={this.state.query}
+				          movieList={this.state.movieList}
+				        />
+			        </div>
+		        </header>
+		        <div className="centerWrapper">
+					<h2>Upcoming</h2>
+				    <MovieCategory url="upcoming"  />
+					<h2>Popular</h2>
+					<MovieCategory url="popular"  />		
+					<h2>Top Rated</h2>
+					<MovieCategory url="top_rated"  />
+				</div>
+          	</div>
           
 			);
 	}
-	getUpcomingMovie(){
 
+	fetchData(){
+		const apiKey = "ff9d34ddaaebff2b1a6100d54346c1a7";
+		let movieLink = "https://api.themoviedb.org/3/search/movie?api_key=" + 
+						apiKey + "&language=en-US&query=" + 
+						this.state.query + "&page=1&include_adult=false";
+		$.ajax({
+			method: 'GET',
+			url: movieLink,
+			success: (movieList) => {
+				this.setState({movieList: movieList.results});
+			}
+		});
+		/**fetch(movieLink).
+		then((Response) => Response.json()).
+		then((findmovie) => {
+			this.setState({movieList: findmovie.results});
+		});**/
 	}
-	getPopularMovie(){
-
-	}
-	//Not sure
+	handleFilter(query){
+		this.setState({
+			query: query
+		}, () => {
+	    if (this.state.query.length > 0) {
+	        return  this.fetchData();
+	    }else if(!this.state.query){
+	      	return;
+	    }
+    });
 	
-
+	}
 }
-
 ReactDOM.render(<MovieApp  />, document.getElementById("main"));
